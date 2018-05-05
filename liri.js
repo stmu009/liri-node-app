f278073dffedf201260ad514a37ef5bdd437a5a5
Damian <dramirez@petmate.com>
2018-05-04 22:50:47 -0500

    added spotify api code

--- liri.js
+++ liri.js
@@ -1,22 +1,10 @@
 var keys = require("./keys.js")
-var inquirer = require("inquirer");
+
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
-// inquirer.prompt([
-//     {
-//       type: "list",
-//       name: "requestType",
-//       message: "LIRI can you do a few things for you, choose one.",
-//       choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
-//     }
-
-//   ]).then(function(request) {
-//       console.log("Request:", request.requestType);
-//   })
 
 var nodeArgs = process.argv;
 
@@ -28,57 +16,60 @@ for (var i = 3; i < nodeArgs.length; i++) {
     requestInput = requestInput + " " + nodeArgs[i];
 }
 
-console.log("request type:", requestType);
+console.log("request me type:", requestType);
 console.log("request input:", requestInput);
 
-var getSpotify = function () {
+var spotifyThis = function () {
+    if (requestInput === ""){requestInput="The Sign Ace of Base"} 
     spotify
         .search({
             type: 'track',
             query: requestInput
         })
         .then(function (response) {
-            console.log(response);
+            var artist = JSON.stringify(response.tracks.items[0].artists[0].name);
+            var song = JSON.stringify(response.tracks.items[0].name);
+            var preview = JSON.stringify(response.tracks.items[0].album.external_urls.spotify);
+            var album = JSON.stringify(response.tracks.items[0].album.name);
+            console.log(JSON.stringify(response.tracks.items[0], null, 2))
+
             console.log(`
-    Artist(s): ${console.log(JSON.stringify(response.tracks, null, 2))}
-    The song's name: ${requestInput}
-    A preview link of the song from Spotify: ${requestInput}
-    The album that the song is from: ${requestInput}`);
+    Artist(s): ${artist}
+    The song's name: ${song}
+    A preview link of the song from Spotify: ${preview}
+    The album that the song is from: ${album}`);
         })
         .catch(function (err) {
             console.log(err);
         })
 };
 
+var myTweets = function(){
+    console.log(`Tweets:`);
+}
+
+var movieThis = function() {
+    console.log("Movie This:");
+}
+
+var doWhat = function() {
+    console.log("do what it says");
+}
+
+
 switch (requestType) {
     case 'my-tweets':
-        console.log(`Tweets:`);
+        myTweets();
         break;
     case 'spotify-this-song':
-        spotify
-            .search({
-                type: 'track',
-                query: requestInput
-            })
-            .then(function (response) {
-                console.log(response);
-                console.log(`
-            Artist(s): ${console.log(JSON.stringify(response.tracks, null, 2))}
-            The song's name: ${requestInput}
-            A preview link of the song from Spotify: ${requestInput}
-            The album that the song is from: ${requestInput}`);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+        spotifyThis();
         break;
     case 'movie-this':
-        console.log("These are your last 20 tweets");
+        movieThis();
         break;
     case 'do-what-it-says':
-        console.log("These are your last 20 tweets");
+        doWhat();
         break;
-
     default:
         console.log("Bad-Request");
         break;
